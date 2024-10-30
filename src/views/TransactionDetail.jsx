cc2639e2b1f854d19cd0da3cbd939036290fd9c5
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-10-30 15:39:13 +0100

    Add related image to transaction detail view

--- src/views/TransactionDetail.jsx
+++ src/views/TransactionDetail.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import jobImage from '../assets/job.png'
+import jobImage from '../assets/transactionDetail/job.png'
+import billsImage from '../assets/transactionDetail/bills.png'
+import foodImage from '../assets/transactionDetail/food.png'
+import transportImage from '../assets/transactionDetail/transport.png'
+import shoppingImage from '../assets/transactionDetail/shopping.png'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { TransactionContext } from '../context/TransactionContext'
+
+const categoryImages = [
+    { category: 'Job', image: jobImage },
+    { category: 'Food', image: foodImage },
+    { category: 'Transport', image: transportImage },
+    { category: 'Shopping', image: shoppingImage },
+    { category: 'Bills', image: billsImage },
+]
 
 const TransactionDetail = () => {
     const { transactionId } = useParams();
@@ -15,10 +27,12 @@ const TransactionDetail = () => {
         return <div>Transaction not found</div>;
     }
 
+    const renderTransactionDetailImage = categoryImages.find(img => img.category === transactionDetail.category);
+
     return (
         <div className='grid grid-cols-2'>
             <div>
-                <img src={jobImage} alt="" />
+                <img src={renderTransactionDetailImage.image} alt="" />
             </div>
             <div className="p-4 border rounded-lg bg-white shadow-md">
                 <h2 className="text-xl font-bold mb-4">Transaction Detail</h2>
