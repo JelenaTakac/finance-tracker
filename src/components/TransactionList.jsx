06d82258d85fa3c18ca2f50cc318c488ef3a7348
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-11-02 20:45:40 +0100

    Update transaction card design

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,6 +1,7 @@
 import { useContext} from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import { Link, useNavigate } from 'react-router-dom';
+import dollarImage from '../assets/dollar.png';
 
 const TransactionList = () => {
     const { transactionState, transactionDispatch } = useContext(TransactionContext);
@@ -17,15 +18,17 @@ const TransactionList = () => {
             {sortTransactions.map(transaction => (
                 <div key={transaction.id} className='grid md:grid-cols-6 py-2 items-center gap-2'>
                     <Link to={`/transactions/${transaction.id}`} className='md:col-span-5 flex p-4 items-center justify-between gap-4 border bg-transparent hover:bg-light rounded-lg shadow-md'>
-                        <div className='text-secondary italic text-xs md:text-base'>{transaction.date}</div>
+                        <img src={dollarImage} alt="Dollar image" className='size-8 md:size-10'/>
+                        {/* <div className='text-secondary italic text-xs md:text-base'>{transaction.date}</div> */}
                         <div>
                             <div className='text-primary font-semibold'>{transaction.description}</div>
-                            <div className='text-secondary italic text-sm md:text-base'>{transaction.category}</div>
+                            <div className='flex gap-2 items-center'>
+                                <div className='text-secondary italic text-xs md:text-base'>{transaction.date}</div>
+                                <div className='text-secondary italic text-xs md:text-base'>{transaction.category}</div>
+                            </div>
                         </div>
-                        <div className='md:text-xl text-primary font-semibold'>
-                            {transaction.type === 'expense' ? '-' : ''} {transaction.amount}$
-                        </div>
-                        <div className='hidden md:block md:text-xl text-primary'>{transaction.type}</div>
+                        <div className='md:text-xl text-primary font-semibold'>{transaction.amount}$</div>
+                        {/* <div className='hidden md:block md:text-xl text-primary'>{transaction.type}</div> */}
                     </Link>
                     <div className='md:col-span-1 flex space-x-4 justify-end'>
                         <button onClick={() => navigate(`/transactionForm/${transaction.id}`)} className='text-primary hover:text-secondary'>Update</button>
